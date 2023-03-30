cd2ca0a682157829e3d43a7c48523c08d5d6eef4
Pierre <pierrelidem@gmail.com>
2023-03-30 16:19:10 +0200

    counter js aboutme

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ const typed = new Typed(".typer", {
   backDelay: 1000,
   loop: true,
 });
+let valueNumbers = document.querySelectorAll(".exp-number");
+let interval = 1000;
+
+valueNumbers.forEach((valueNumber) => {
+  let startValue = 0;
+  let endValue = parseInt(valueNumber.getAttribute("data-val"));
+
+  let duration = Math.floor(interval / endValue);
+  
+  let counter = setInterval(() => {
+    startValue += 1;
+    valueNumber.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
