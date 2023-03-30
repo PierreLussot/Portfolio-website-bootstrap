49d4b0ace11a4d805d68f22d237a7c65e87f3f59
Pierre <pierrelidem@gmail.com>
2023-03-30 15:04:12 +0200

    section

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed(".typer", {
+  strings: [
+    "Html <i class='bi bi-filetype-html'></i>",
+    "Css <i class='bi bi-filetype-css'></i>",
+    "Php <i class='bi bi-filetype-php'></i>",
+    "Javascript <i class='bi bi-filetype-js'></i>",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
